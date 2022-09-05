58d73af1f58e180f29918172854be2ae3fb90f8b
iNFiENiTE-World <infienite@gmail.com>
2022-09-05 18:34:29 +0800

    Folder structure updated, added new folders in explore, contains new HTML file for website

--- commonUtil.js
+++ commonUtil.js
@@ -3,7 +3,7 @@
 /**
  * Opens sidebar menu inside the header component
  * @since 2nd September 2022
- * @author Khilfi
+ * @author The Badminton Square Team
  */
 function sidebarOpen() {
     document.getElementById("mySidebar").style.width = "240px";
@@ -22,6 +22,7 @@ function sidebarOpen() {
 /**
  * Closes sidebar menu inside the header component
  * @since 2nd September 2022
+ * @author The Badminton Square Team
  */
 function sidebarClose() {
     document.getElementById("mySidebar").style.width = "0";
